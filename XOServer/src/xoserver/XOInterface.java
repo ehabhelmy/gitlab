c17d161d8edeb3db8fd17e55500afd69e1d3309f
medhat <ehabmahmoudhelmy@gmail.com>
2017-02-21 14:34:33 +0200

    formated

--- XOServer/src/xoserver/XOInterface.java
+++ XOServer/src/xoserver/XOInterface.java
@@ -12,23 +12,26 @@ import java.rmi.RemoteException;
  *
  * @author michael
  */
-public interface XOInterface extends Remote{
-    
-    public void Reset()throws RemoteException;
- 
-    public void changeValueM1()throws RemoteException;
+public interface XOInterface extends Remote {
 
-    public void changeValueM6()throws RemoteException;
-    public void changeValueM7()throws RemoteException;
-    public void changeValueM8()throws RemoteException;
-    public void changeValueM9()throws RemoteException;
+    public void Reset() throws RemoteException;
 
-    public void changeValueM2()throws RemoteException;
-    
-    public void changeValueM3()throws RemoteException;
-    
-    public void changeValueM4()throws RemoteException;
-    
-    public void changeValueM5()throws RemoteException;
-    
+    public void changeValueM1() throws RemoteException;
+
+    public void changeValueM6() throws RemoteException;
+
+    public void changeValueM7() throws RemoteException;
+
+    public void changeValueM8() throws RemoteException;
+
+    public void changeValueM9() throws RemoteException;
+
+    public void changeValueM2() throws RemoteException;
+
+    public void changeValueM3() throws RemoteException;
+
+    public void changeValueM4() throws RemoteException;
+
+    public void changeValueM5() throws RemoteException;
+
 }
