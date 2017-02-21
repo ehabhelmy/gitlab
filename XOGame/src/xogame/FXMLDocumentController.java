2fc447b8be663043af632efd7f6882b18b5da8a5
medhat <ehabmahmoudhelmy@gmail.com>
2017-02-21 13:51:04 +0200

    game version1

---
+++ XOGame/src/xogame/FXMLDocumentController.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xogame;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author ehab
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private TextField m8;
+    @FXML
+    private TextField m7;
+    @FXML
+    private TextField m6;
+    @FXML
+    private TextField m5;
+    @FXML
+    private TextField m4;
+    @FXML
+    private TextField m3;
+    @FXML
+    private TextField m2;
+    @FXML
+    private TextField m1;
+    @FXML
+    private TextField m9;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void resetGame(ActionEvent event) {
+    }
+    
+}
